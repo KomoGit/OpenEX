ed4781bb9a9f969a52c3bc1712b16764d83e65d6
James <entreur@gmail.com>
2023-04-13 17:16:06 +0400

    New door object script.

--- Assets/Scripts/World/Door.cs
+++ Assets/Scripts/World/Door.cs
@@ -5,22 +5,21 @@ public class Door : MonoBehaviour, IInteractive
 {
     [SerializeField] private float _speed = 2f;
     [SerializeField] private float _angle = 90f;
-    [SerializeField] private bool _isOpen = false;
     [SerializeField] private bool _isLocked = false;
-    [SerializeField] AudioClip OpenDoor = default;
-    [SerializeField] AudioClip CloseDoor = default;
+    [SerializeField] private AudioSource _audioSource;
+    [SerializeField] private AudioClip OpenDoor = default;
+    [SerializeField] private AudioClip CloseDoor = default;
+    //public event EventHandler RemoteOpenDoor;
+    private bool _isOpen = false;
     private Quaternion _startRotation;
     private Quaternion _endRotation;
-
     private Coroutine _coroutine;
 
     private void Awake()
     {  
         _startRotation = transform.rotation;
         _endRotation = _startRotation * Quaternion.Euler(0, _angle, 0);
-    }
-    
-
+    }   
     public void Activate()
     {
         if (!_isLocked && !IsActivated())
@@ -28,12 +27,12 @@ public class Door : MonoBehaviour, IInteractive
             if(_coroutine != null)  
             {
                 StopCoroutine(_coroutine);
-            }
+            }        
             _coroutine = StartCoroutine(RotateDoor(_endRotation));
+            PlaySound(OpenDoor);
             _isOpen = true;
         }
     }
-
     public void Deactivate()
     {
         if (!_isLocked && IsActivated())
@@ -41,8 +40,9 @@ public class Door : MonoBehaviour, IInteractive
             if (_coroutine != null)
             {
                 StopCoroutine(_coroutine);
-            }
+            }           
             _coroutine = StartCoroutine(RotateDoor(_startRotation));
+            PlaySound(CloseDoor);
             _isOpen = false;
         }
     }
@@ -55,9 +55,16 @@ public class Door : MonoBehaviour, IInteractive
             yield return null;
         }
     }
-
     public bool IsActivated()
     {
         return _isOpen;
     }
+    private void PlaySound(AudioClip clip)
+    {
+        _audioSource.PlayOneShot(clip);
+    }
+    //protected virtual void OnRemoteOpenDoor()
+    //{
+    //    RemoteOpenDoor?.Invoke(this, EventArgs.Empty);
+    //}
 }
