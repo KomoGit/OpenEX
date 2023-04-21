ad74e9dc908213a25bd8fa645fcc6814737f9731
James <entreur@gmail.com>
2023-04-21 13:13:59 +0400

    Agility now works, few features missing

--- Assets/Scripts/Player/CharactherAbilities/Agility.cs
+++ Assets/Scripts/Player/CharactherAbilities/Agility.cs
@@ -4,21 +4,29 @@ using UnityEngine;
 public class Agility : MonoBehaviour,IAbility
 {
     [SerializeField] private AbilityManager AbilityManager;
-    //[SerializeField] private P_movement PlayerMovement;
+    [SerializeField] private P_movement PlayerMovement;
     [SerializeField] private float DrainRatePerSecond;
     [SerializeField] private AudioClip AgilitySFX;
     [SerializeField] [Range(1,4)] private int abilityLevel = 1;
     private bool AgilityEnabled = false;
+    
+    private float PlayerJumpForce;
+    private float PlayerSpeed;
+
+    void Awake()
+    {
+        PlayerJumpForce = PlayerMovement.CurrentJumpForce;
+        PlayerSpeed = PlayerMovement.CurrentMovementSpeed;
+    }
 
     void Start()
     {
-        //PlayerMovement = FindObjectOfType<P_movement>();
+        PlayerMovement = FindObjectOfType<P_movement>();
         AbilityManager = FindObjectOfType<AbilityManager>();    
     }
-
     public void AbilityActivate()
     {
-        if (AbilityManager.IsEnergyDepleted())
+        if (AbilityManager.EnergyDepleted())
         {
             return;
         }
@@ -28,20 +36,19 @@ public class Agility : MonoBehaviour,IAbility
             {
                 Debug.Log("Agility Activated");
                 AbilityManager.SecondPassed += DrainPerSecond;
-                AgilityEnabled = true;
+                EnableAgility();
             }
-            else
+            else if (AgilityEnabled == true || AbilityManager.EnergyDepleted())
             {
                 Debug.Log("Agility Deactivated");
                 AbilityManager.SecondPassed -= DrainPerSecond;
-                AgilityEnabled = false;
+                DisableAgility();   
             }
         }
     }
-
     private void DrainPerSecond(object sender, EventArgs e)
     {
-        if (!AbilityManager.IsEnergyDepleted())
+        if (!AbilityManager.EnergyDepleted())
         {
             AbilityManager.DrainEnergy(DrainRatePerSecond);
         }
@@ -49,5 +56,36 @@ public class Agility : MonoBehaviour,IAbility
         {
             AgilityEnabled = false;
         }
+    }
+
+    private void EnableAgility()
+    {
+        AgilityEnabled = true;
+        switch (abilityLevel)
+        {
+            case 2:
+                PlayerMovement.CurrentMovementSpeed *= 2;
+                PlayerMovement.CurrentJumpForce *= 2;
+                //Add fall damage reducer as well. For all cases
+                break;
+            case 3:
+                PlayerMovement.CurrentMovementSpeed *= 3;
+                PlayerMovement.CurrentJumpForce *= 3;
+                break;
+            case 4:
+                PlayerMovement.CurrentMovementSpeed *= 4;
+                PlayerMovement.CurrentJumpForce *= 4;
+                break;
+            default:
+                PlayerMovement.CurrentMovementSpeed *= 2f;
+                PlayerMovement.CurrentJumpForce *= 2f;
+                break;
+        }
+    }
+    private void DisableAgility()
+    {
+        PlayerMovement.CurrentJumpForce = PlayerJumpForce;
+        PlayerMovement.CurrentMovementSpeed = PlayerSpeed;
+        AgilityEnabled = false;
     }
 }
