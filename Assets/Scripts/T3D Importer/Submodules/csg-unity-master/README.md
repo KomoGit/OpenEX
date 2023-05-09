cb84384cd0a8b2328cc2278522b4d67a4f302739
James <entreur@gmail.com>
2023-05-09 12:45:36 +0400

    Removing plugin T3D from REPO

--- Assets/Scripts/T3D Importer/Submodules/csg-unity-master/README.md
+++
@@ -1,6 +0,0 @@
-## Constructive Solid Geometry for Unity 3D
-
-- Union, Subtraction, Difference
-- Simple Planar Mapping
-- Works with Concave Meshes
-- Uses Unity 3D Meshes
--- Assets/Scripts/T3D Importer/Submodules/csg-unity-master/editor/csg/BrushBuilder.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using System.Collections;
-
-public abstract class BrushBuilder {
-
-	// Use this for initialization
-	public abstract void Build();
-	
-}
