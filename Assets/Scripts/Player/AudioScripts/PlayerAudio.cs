bb96cb363110d37efcb316c77f8e07bcbb69996a
James <entreur@gmail.com>
2023-05-15 11:56:28 +0400

    Ensuring all vars are named in similar fashion

--- Assets/Scripts/Player/AudioScripts/PlayerAudio.cs
+++ Assets/Scripts/Player/AudioScripts/PlayerAudio.cs
@@ -3,28 +3,27 @@ using UnityEngine;
 public class PlayerAudio : MonoBehaviour
 {
     [Header("Audio Parameters")]
-    [SerializeField] private AudioSource footstepAudioSource = default;
-    [SerializeField] private P_movement playerScript;
-    [SerializeField] private float baseStepSpeed = 0.5f;
-    [SerializeField] private float crouchStepMultiplier = 1.5f;
+    [SerializeField] private AudioSource FootstepAudioSource = default;
+    [SerializeField] private P_movement PlayerScript;
+    [SerializeField] private float BaseStepSpeed = 0.5f;
+    [SerializeField] private float CrouchStepMultiplier = 1.5f;
     [Header("Audio Clips")]
-    [SerializeField] AudioClip[] carpetClips = default;
-    [SerializeField] AudioClip[] grassClips = default;
-    [SerializeField] AudioClip[] metalClips = default;
-    [SerializeField] AudioClip[] stoneClips = default;
-    [SerializeField] AudioClip[] tileClips = default;
-    [SerializeField] AudioClip[] woodClips = default;
-    [SerializeField] AudioClip[] waterClips = default;
+    [SerializeField] AudioClip[] CarpetClips = default;
+    [SerializeField] AudioClip[] GrassClips = default;
+    [SerializeField] AudioClip[] MetalClips = default;
+    [SerializeField] AudioClip[] StoneClips = default;
+    [SerializeField] AudioClip[] TileClips = default;
+    [SerializeField] AudioClip[] WoodClips = default;
+    [SerializeField] AudioClip[] WaterClips = default;
 
     public bool FootstepsEnabled { get; set; }
-    private float footstepTimer = 0f;
-    private float GetCurrentOffset => playerScript.IsCrouching || playerScript.IsSilentWalking ? baseStepSpeed * crouchStepMultiplier
-                                      : baseStepSpeed;
+    private float FootstepTimer = 0f;
+    private float GetCurrentOffset => PlayerScript.IsCrouching || PlayerScript.IsSilentWalking ? BaseStepSpeed * CrouchStepMultiplier
+                                      : BaseStepSpeed;
     private void Awake()
     {
-        playerScript = FindObjectOfType<P_movement>();
+        PlayerScript = FindObjectOfType<P_movement>();
     }
-
     // Update is called once per frame
     void Update()
     {
@@ -34,47 +33,47 @@ public class PlayerAudio : MonoBehaviour
     //by the AbilityManager to check if there is input.
     private void HandleFootsteps()
     {
-        if (!playerScript.IsGrounded) return;
-        if (!playerScript.IsWalking) return;
+        if (!PlayerScript.IsGrounded) return;
+        if (!PlayerScript.IsWalking) return;
 
-        footstepTimer -= Time.deltaTime;
+        FootstepTimer -= Time.deltaTime;
 
-        if(footstepTimer <= 0)
+        if(FootstepTimer <= 0)
         {
             //Perhaps we can create a single raycast function that everything that requires it access it.
             //After this is working, I will see to that.
-            if (Physics.Raycast(playerScript.orientation.position, Vector3.down,out RaycastHit hit,3))//AVOID MAGIC NUMBER.
+            if (Physics.Raycast(PlayerScript.orientation.position, Vector3.down,out RaycastHit hit,3))//AVOID MAGIC NUMBER.
             {
                 switch(hit.collider.tag)
                 {
                     case "Footsteps/WOOD":
-                        footstepAudioSource.PlayOneShot(woodClips[Random.Range(0,woodClips.Length -1)]);
+                        FootstepAudioSource.PlayOneShot(WoodClips[Random.Range(0,WoodClips.Length -1)]);
                         break;
                     case "Footsteps/GRASS":
-                        footstepAudioSource.PlayOneShot(grassClips[Random.Range(0, grassClips.Length - 1)]);
+                        FootstepAudioSource.PlayOneShot(GrassClips[Random.Range(0, GrassClips.Length - 1)]);
                         break;
                     case "Footsteps/STONE":
-                        footstepAudioSource.PlayOneShot(stoneClips[Random.Range(0, stoneClips.Length - 1)]);
+                        FootstepAudioSource.PlayOneShot(StoneClips[Random.Range(0, StoneClips.Length - 1)]);
                         break;
                     case "Footsteps/METAL":
-                        footstepAudioSource.PlayOneShot(metalClips[Random.Range(0, metalClips.Length - 1)]);
+                        FootstepAudioSource.PlayOneShot(MetalClips[Random.Range(0, MetalClips.Length - 1)]);
                         break;
                     case "Footsteps/TILE":
-                        footstepAudioSource.PlayOneShot(tileClips[Random.Range(0, tileClips.Length - 1)]);
+                        FootstepAudioSource.PlayOneShot(TileClips[Random.Range(0, TileClips.Length - 1)]);
                         break;
                     case "Footsteps/CARPET":
-                        footstepAudioSource.PlayOneShot(carpetClips[Random.Range(0, carpetClips.Length - 1)]);
+                        FootstepAudioSource.PlayOneShot(CarpetClips[Random.Range(0, CarpetClips.Length - 1)]);
                         break;
                     case "Footsteps/WATER":
-                        footstepAudioSource.PlayOneShot(waterClips[Random.Range(0, waterClips.Length - 1)]);
+                        FootstepAudioSource.PlayOneShot(WaterClips[Random.Range(0, WaterClips.Length - 1)]);
                         break;
                     default:
                         Debug.Log("Warning, no tag has been added to this object.");
-                        footstepAudioSource.PlayOneShot(stoneClips[Random.Range(0, stoneClips.Length - 1)]);
+                        FootstepAudioSource.PlayOneShot(StoneClips[Random.Range(0, StoneClips.Length - 1)]);
                         break;
                 }
             }
-            footstepTimer = GetCurrentOffset;
+            FootstepTimer = GetCurrentOffset;
         }
     }
 }
