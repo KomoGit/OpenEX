352b42f8e42ec8bb5ec9f427cb7bb34e8c57e148
James <entreur@gmail.com>
2023-05-15 12:02:58 +0400

    Removed commented code

--- Assets/Scripts/Player/Controls/Cam_position.cs
+++ Assets/Scripts/Player/Controls/Cam_position.cs
@@ -5,11 +5,9 @@ using UnityEngine;
 [RequireComponent(typeof(P_Interact))]
 public class Cam_position : MonoBehaviour
 {
-    [SerializeField] private Transform _player;
+    [SerializeField] private Transform Player;
     void Update()
     {
-        transform.position = _player.position;
-        //Vector3 newPosition = new(_player.position.x, transform.position.y, _player.position.z);
-        //transform.position = newPosition;
+        transform.position = Player.position;
     }
 }
