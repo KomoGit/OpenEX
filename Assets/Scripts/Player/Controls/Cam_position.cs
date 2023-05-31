18983690c066635ca8c148caac02474a1f77df38
James <entreur@gmail.com>
2023-05-31 18:41:56 +0400

    .

--- Assets/Scripts/Player/Controls/Cam_position.cs
+++ Assets/Scripts/Player/Controls/Cam_position.cs
@@ -1,8 +1,4 @@
 using UnityEngine;
-
-[RequireComponent(typeof(Flashlight))]
-[RequireComponent(typeof(CarryObject))]
-[RequireComponent(typeof(P_Interact))]
 public class Cam_position : MonoBehaviour
 {
     [SerializeField] private Transform Player;
