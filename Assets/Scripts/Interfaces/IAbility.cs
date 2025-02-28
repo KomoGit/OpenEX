6c412cd03d0e07b62c41c1794efb946f5b2cd070
Elnur <63106963+KomoGit@users.noreply.github.com>
2025-02-28 17:50:02 +0400

    Removed DrainEnergy as it is not used

--- Assets/Scripts/Interfaces/IAbility.cs
+++ Assets/Scripts/Interfaces/IAbility.cs
@@ -2,11 +2,5 @@ using System;
 
 public interface IAbility 
 {
-    public void AbilityActivate();
-    /// <summary>
-    /// Drains Energy Per Second.
-    /// </summary>
-    /// <param name="sender"></param>
-    /// <param name="e"></param>
-    private void DrainEnergy(object sender, EventArgs e) { }
+    void AbilityActivate();
 }
